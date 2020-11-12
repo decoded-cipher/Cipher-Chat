19a45ad57417237e3fc3b48043cb719736229196
Arjun Krishna <arjun12345krishna@gmail.com>
2020-11-13 00:43:10 +0530

    Barely Started

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,18 @@
   export default {
     name: 'App',
     components: {
-      
+
     }
   }
 </script>
 
 <style>
-#app {
-  font-family: 'Nunito', sans-serif;
-}
+  #app {
+    font-family: 'Nunito', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    /* width: 100vw; */
+  }
 </style>
